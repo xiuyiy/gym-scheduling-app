4e4753e28de06c8d69075c03ba1e314bd3035c2a
Ye, Xiuyi <xiye@microstrategy.com>
2018-02-24 15:58:15 -0500

    1. created json object

---
+++ controllers/classController.js
@@ -0,0 +1,101 @@
+var classSchecule = [{},//11
+    {
+        "title": "WEIGHT TRAINING",
+        "instructor": "KEVIN",
+        "startTime": "7:00 AM",
+        "duration": "45 MIN"
+    },//12
+    {
+        "title": "TOTAL BODY",
+        "instructor": "KEVIN",
+        "startTime": "7:00 AM",
+        "duration": "45 MIN"
+    },//13
+    {
+        "title": "WEIGTH TRAINING",
+        "instructor": "KEVIN",
+        "startTime": "7:00 AM",
+        "duration": "45 MIN"
+    },//14
+    {
+        "title": "CORE STRENGTH",
+        "instructor": "KEVIN",
+        "startTime": "7:00 AM",
+        "duration": "45 MIN"
+    },//15
+
+    {
+        "title": "TOTAL BODY",
+        "instructor": "KEVIN",
+        "startTime": "12:00 PM",
+        "duration": "45 MIN"
+    },//21
+    {
+        "title": "VINYASA YOGA",
+        "instructor": "YVETTE",
+        "startTime": "12:00 PM",
+        "duration": "45 MIN"
+    },//22
+    {
+        "title": "50/50",
+        "instructor": "HEIDI",
+        "startTime": "12:00 PM",
+        "duration": "45 MIN"
+    },//23
+    {
+        "title": "VINYASA YOGA",
+        "instructor": "YVETTE",
+        "startTime": "12:00 PM",
+        "duration": "45 MIN"
+    },//24
+    {
+        "title": "TABATA",
+        "instructor": "IULIIA",
+        "startTime": "12:00 PM",
+        "duration": "45 MIN"
+    },//25
+
+    {}, {}, {}, {}, {},//31-35
+
+    {},//41
+    {
+        "title": "BARBELL STRENGTH",
+        "ps": "20 people max"
+        "instructor": "IULIIA",
+        "startTime": "5:15 PM",
+        "duration": "55 MIN"
+    }, //42
+    {}, //43
+    {
+        "title": "BARBELL STRENGTH",
+        "ps": "20 people max"
+        "instructor": "IULIIA",
+        "startTime": "5:15 PM",
+        "duration": "55 MIN"
+    }, //44
+    {}, //45
+    {
+        "title": "BODY EXTREME",
+        "instructor": "IULIIA",
+        "startTime": "6:00 PM",
+        "duration": "45 MIN"
+    }, //51
+    {
+        "title": "SPORTS CONDITIONING",
+        "instructor": "IULIIA",
+        "startTime": "6:10 PM",
+        "duration": "45 MIN"
+    }, //52
+    {
+        "title": "HIT BOOT CAMP",
+        "instructor": "IULIIA",
+        "startTime": "6:00 PM",
+        "duration": "45 MIN"
+    }, //53
+    {
+        "title": "Zumba",
+        "instructor": "CHONA",
+        "startTime": "6:10 PM",
+        "duration": "45 MIN"
+    } //54
+];
