8292855fd13519e53c00cd3822837c8b02c573cf
He, Ming <mhe@microstrategy.com>
2018-02-24 16:41:47 -0500

    add bootstrap and reserve js

--- controllers/ReserveController.js
+++ controllers/ReserveController.js
@@ -4,14 +4,56 @@ Created by Ming He on Feb 24, 2018
 
 (function () {
 
-    var injectParams = ['$scope', '$rootScope'];
+    var injectParams = ['$scope', '$rootScope', '$http'];
 
-    var signupController = function ($scope, $rootScope) {
+    var reserveController = function ($scope, $rootScope, $http) {
+        var currentDate = new Date();
+        var className = "Barbell";
 
-    }
+        $scope.totalNumber = 20;
 
-    signupController.$inject = injectParams;
+        $scope.generateReservations = function() {
+            $scope.reservations = [];
+            for (var i = 0; i < $socpe.totalNumber; i++) {
+                $scope.reservations[i] = {
+                    "name": null,
+                    "employeeId": null
+                };
+            }
+        };
 
-    angular.module('gym-schedule-app').controller('signupController', signupController);
+        $scope.reserveASpot = function(name, employeeId, number) {
+            if ($scope.reservations.some(function(element){
+                return (element.employeeId === employeeId);
+            })) {
+                $scope.reserveSuc = false;
+                $scope.reserveMessage = "Sorry you've already reserved.";
+            } else {
+                $scope.reservations[number] = {
+                    "name": name,
+                    "employeeId": employeeId
+                }
+                $scope.reserveSuc = true;
+                $scope.reserveMessage = "Your just reserved successfully!";
+            }
+        };
+
+        $scope.formatDate = function (date) {
+            var d = new Date(date),
+                month = '0' + (d.getMonth() + 1),
+                day = '0' + d.getDate(),
+                year = d.getFullYear();
+
+            month = month.slice(month.length - 2);
+            day = day.slice(day.length - 2);
+
+            return [year, month, day].join('-');
+        }
+
+    };
+
+    reserveController.$inject = injectParams;
+
+    angular.module('gym-schedule-app').controller('reserveController', reserveController);
 
 }());
