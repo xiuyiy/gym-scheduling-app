e343d93b9fd54c237f7d3935fffd5d7864cf4dd9
He, Ming <mhe@microstrategy.com>
2018-02-26 22:24:08 -0500

    add send email function

--- express-server/server.js
+++ express-server/server.js
@@ -5,6 +5,7 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 var Factory = require("./module.factory.js");
 var bodyParser = require('body-parser');
+var nodemailer = require("nodemailer");
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -33,3 +34,61 @@ app.post('/reservations', function(req, res) {
 
 app.listen(3000);
 console.log('Listening on port 3000...');
+
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "mstr.noreply@gmail.com",
+        pass: "mstr123456"
+    }
+});
+var rand,mailOptions,host,link;
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
+
+app.get('/send',function(req,res){
+    rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+     });
+});
+
+app.get('/verify',function(req,res){
+console.log(req.protocol+":/"+req.get('host'));
+if((req.protocol+"://"+req.get('host')) == ("http://"+host)) {
+    console.log("Domain is matched. Information is from Authentic email");
+    if(req.query.id==rand)
+    {
+        console.log("email is verified");
+        res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+    }
+    else
+    {
+        console.log("email is not verified");
+        res.end("<h1>Bad Request</h1>");
+    }
+} else {
+    res.end("<h1>Request is from unknown source");
+}
+});
+
+/*--------------------Routing Over----------------------------*/
